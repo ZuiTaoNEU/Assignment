4446d585c818be9d2a66efe3c8e5466eeb969cc8
Zui Tao <145932024+ZuiTaoNEU@users.noreply.github.com>
2024-01-28 22:01:15 -0500

    Add files via upload

---
+++ A1/src/model/Person.java
@@ -0,0 +1,200 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author EHY
+ */
+public class Person {
+    
+    private String name;
+    private String age;
+    private String gender;
+    private String weight;
+    private String tall;
+    private String Caddress;
+    private String Czip;
+    private String Ctele;
+    private String Cstate;
+    private String Ccountry;
+    private String Waddress;
+    private String Wzip;
+    private String Wtele;
+    private String Wstate;
+    private String Wcountry;
+    private String accnumber;
+    private String accname;
+    private String type;
+    private String debetnum;
+    private String spendnum;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    public String getTall() {
+        return tall;
+    }
+
+    public void setTall(String tall) {
+        this.tall = tall;
+    }
+
+    public String getCaddress() {
+        return Caddress;
+    }
+
+    public void setCaddress(String Caddress) {
+        this.Caddress = Caddress;
+    }
+
+    public String getCzip() {
+        return Czip;
+    }
+
+    public void setCzip(String Czip) {
+        this.Czip = Czip;
+    }
+
+    public String getCtele() {
+        return Ctele;
+    }
+
+    public void setCtele(String Ctele) {
+        this.Ctele = Ctele;
+    }
+
+    public String getCstate() {
+        return Cstate;
+    }
+
+    public void setCstate(String Cstate) {
+        this.Cstate = Cstate;
+    }
+
+    public String getCcountry() {
+        return Ccountry;
+    }
+
+    public void setCcountry(String Ccountry) {
+        this.Ccountry = Ccountry;
+    }
+
+    public String getWaddress() {
+        return Waddress;
+    }
+
+    public void setWaddress(String Waddress) {
+        this.Waddress = Waddress;
+    }
+
+    public String getWzip() {
+        return Wzip;
+    }
+
+    public void setWzip(String Wzip) {
+        this.Wzip = Wzip;
+    }
+
+    public String getWtele() {
+        return Wtele;
+    }
+
+    public void setWtele(String Wtele) {
+        this.Wtele = Wtele;
+    }
+
+    public String getWstate() {
+        return Wstate;
+    }
+
+    public void setWstate(String Wstate) {
+        this.Wstate = Wstate;
+    }
+
+    public String getWcountry() {
+        return Wcountry;
+    }
+
+    public void setWcountry(String Wcountry) {
+        this.Wcountry = Wcountry;
+    }
+
+    public String getAccnumber() {
+        return accnumber;
+    }
+
+    public void setAccnumber(String accnumber) {
+        this.accnumber = accnumber;
+    }
+
+    public String getAccname() {
+        return accname;
+    }
+
+    public void setAccname(String accname) {
+        this.accname = accname;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDebetnum() {
+        return debetnum;
+    }
+
+    public void setDebetnum(String debetnum) {
+        this.debetnum = debetnum;
+    }
+
+    public String getSpendnum() {
+        return spendnum;
+    }
+
+    public void setSpendnum(String spendnum) {
+        this.spendnum = spendnum;
+    }
+
+    
+}
+    
+    
+    
+
+    
